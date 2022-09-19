91b6d5e4704d5c0f33ac2d676b2f5566bab7b874
AAdler <945680791@qq.com>
2022-09-19 13:38:24 +0800

    commit 0919

--- js/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV == "production" ? process.env.CONNECT_URL : "/",
-  outputDir: "../files/static2",
-  devServer: {
-    proxy: process.env.PROXY_URL
-  }
-};
